f52a72ce53a9442309a584ceea57388ed857c9c3
Peyton Vaughn <pvaughn@6fusion.com>
2017-12-21 16:59:05 -0500

    hmm

--- bin/meter-connector.rb
+++ bin/meter-connector.rb
@@ -13,7 +13,7 @@ max_threads = Integer(ENV['METER_API_THREADS'] || 10)
 thread_pool = Concurrent::ThreadPoolExecutor.new(min_threads: 1, max_threads: max_threads, max_queue: max_threads * 2, fallback_policy: :caller_runs)
 Machine.distinct(:uuid).each do |uuid|
   machine = Machine.where({uuid: uuid}).first
-  thread_pool.post { machine.submit_create unless machine.already_submitted? }
+  thread_pool.post { machine.submit_create }
 end
 
 thread_pool.shutdown
